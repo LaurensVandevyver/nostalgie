c66dfa484267f67900aa9243a9ad39970127e2cc
wheretwo <hello@wheretwo.com>
2017-03-28 15:09:43 +0200

    dynamische afbeeldingen

--- src/js/script.js
+++ src/js/script.js
@@ -3,92 +3,90 @@
 import React from 'react';
 import {render} from 'react-dom';
 import App from './containers/App';
-import App2 from './containers/App2';
+// import App2 from './containers/App2';
+
+const active = 0;
+
+const onScroll = () => {
+  window.requestAnimationFrame(() => {
+    focusElements();
+  });
+};
+
+const focusElements = () => {
+  const center = getCenterElement();
+  if (center !== active) {
+    focusSection(center);
+    focusImage(center);
+  }
+};
+
+const focusSection = id => {
+  const $prevSection = document.querySelector(`model.focus`);
+  if ($prevSection) {
+    $prevSection.classList.remove(`focus`);
+  }
+
+  const $nextSection = document.querySelector(`.model[data-id="${id}"]`);
+  $nextSection.classList.add(`focus`);
+};
+
+const focusImage = id => {
+  const $prevImg = document.querySelector(`img.focus`);
+  if ($prevImg) {
+    $prevImg.classList.remove(`focus`);
+  }
+
+  const $nextImg = document.querySelector(`img[data-id="${id}"]`);
+  $nextImg.classList.add(`focus`);
+};
+
+const getCenterElement = () => {
+  const sections = document.querySelectorAll(`.model`);
+  const center = window.innerHeight * 0.4;
+  let id = 1;
+
+  sections.forEach($section => {
+    const top = $section.getBoundingClientRect().top;
+    const bottom = $section.getBoundingClientRect().bottom;
+    if (center > top && center < bottom) {
+      id = parseInt($section.dataset.id);
+    }
+  });
+
+  return id;
+};
 
 const init = () => {
-
-  const songs = [
-    {
-      image: `../assets/img/album1.jpg`,
-      url: `../assets/audio/one.mp3`
-    },
-    {
-      image: `../assets/img/album2.jpg`,
-      url: `../assets/audio/two.mp3`
-    },
-    {
-      image: `../assets/img/album3.jpg`,
-      url: `../assets/audio/three.mp3`
-    }
-  ];
 
   render (
     <App />,
     document.querySelector(`.react-mount`),
-    <App2 song={songs[0]} />,
-    document.querySelector(`.react-mount2`),
-    <App2 song={songs[1]} />,
-    document.querySelector(`.react-mount3`),
-    <App2 song={songs[3]} />,
-    document.querySelector(`.react-mount4`)
+    // <App2 song={songs[0]} />,
+    // document.querySelector(`.react-mount2`),
+    // <App2 song={songs[1]} />,
+    // document.querySelector(`.react-mount3`),
+    // <App2 song={songs[3]} />,
+    // document.querySelector(`.react-mount4`)
    );
+
+  window.addEventListener(`scroll`, onScroll);
+
+  // const songs = [
+  //   {
+  //     image: `../assets/img/album1.jpg`,
+  //     url: `../assets/audio/one.mp3`
+  //   },
+  //   {
+  //     image: `../assets/img/album2.jpg`,
+  //     url: `../assets/audio/two.mp3`
+  //   },
+  //   {
+  //     image: `../assets/img/album3.jpg`,
+  //     url: `../assets/audio/three.mp3`
+  //   }
+  // ];
 
 };
 
 init();
-
-// {
-//
-//   const model = document.querySelector(`.modelsection`);
-//   let active = 0;
-//
-//   const onScroll = () => {
-//     window.requestAnimationFrame(() => {
-//       focusElements();
-//     });
-//   };
-//
-//   const focusElements = () => {
-//     const center = getCenterElement();
-//     if (center !== active) {
-//       focusImage(center);
-//       active = center;
-//     }
-//   };
-//
-//   const getCenterElement = () => {
-//     const sections = document.querySelectorAll(`.model`);
-//     const center = model.innerHeight * 0.4;
-//     console.log(center);
-//     const id = 1;
-//
-//     sections.forEach($section => {
-//       const top = $section.getBoundingClientRect().top;
-//       const bottom = $section.getBoundingClientRect().bottom;
-//       if (center > top && center < bottom) {
-//         const id = parseInt($section.dataset.id);
-//         console.log(id);
-//       }
-//     });
-//     return id;
-//   };
-//
-//   const focusImage = id => {
-//     const $prevImg = document.querySelector(`img.focus`);
-//     if ($prevImg) {
-//       $prevImg.classList.remove(`focus`);
-//     }
-//
-//     const $nextImg = document.querySelector(`img[data-id="${id}"]`);
-//     $nextImg.classList.add(`focus`);
-//   };
-//
-//   const init = () => {
-//     const height = parseInt(model.height);
-//     console.log(height);
-//     window.addEventListener(`scroll`, onScroll);
-//   };
-//
-//   init();
-//
-// }
